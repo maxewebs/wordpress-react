c02e13187481d9a9b324364c74e91a92e1bd6876
Maximiliano <maxewebs@gmail.com>
2020-12-05 11:31:02 +0100

    added image and excerpt

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,9 +14,11 @@ const IndexPage = ({data}) => (
       <section className="posts-container">
         {data.allWpPost.nodes.map((post) => (
           <div className="post-preview" key={post.slug}>
+            <Image className="featured-image" fluid={post.featuredImage.node.localFile.childImageSharp.fluid} />
             <Link to={post.slug}>
               {post.title}
             </Link>
+            <div className='excerpt' dangerouslySetInnerHTML={{ __html: post.excerpt }} />
           </div>
         ))}
       </section>
@@ -27,12 +29,34 @@ const IndexPage = ({data}) => (
 export default IndexPage
 
 export const pageQuery = graphql`
-  query {
-    allWpPost(sort: { fields: [date] }) {
-        nodes {
-            title
-            slug
-        }
-    }
+query {
+  allWpPost(sort: { fields: [date] }) {
+      nodes {
+          title
+          excerpt
+          slug
+          featuredImage {
+            node {
+              localFile {
+                childImageSharp {
+                  fluid {
+                    src
+                    aspectRatio
+                    base64
+                    originalImg
+                    originalName
+                    presentationHeight
+                    presentationWidth
+                    sizes
+                    srcSet
+                    srcSetWebp
+                    srcWebp
+                  }
+                }
+              }
+            }
+          }
+      }
   }
+}
 `
